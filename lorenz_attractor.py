00f51f22cc0868327c9bdc06db303351cbf7e150
jeanlaunay <jealauorl@gmail.com>
2020-02-12 18:18:04 +0000

    Lorenz Attractor modelled with RK

--- lorenz_attractor.py
+++ lorenz_attractor.py
@@ -1,12 +1,71 @@
 import numpy as np
 
-class lorenz:
-    def _init_(self):
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-        self.dt = 0.001 #default step size
+
+class lorenz:
+    def __init__(self):
+
+        self.dt = 0.01 #default step size
         self.T = 100 # default final time
-        self.N = self.T/self.dt # default number of time steps
-        self.X = np.zeros((3,self.N)) # array to store X positions (x,y,z)
+        self.N = int(self.T/self.dt) # default number of time steps
+
+        self.X = np.zeros((self.N,3),dtype=float) # array to store X positions (x,y,z)
+
+        self.K1 = np.zeros((1,3),dtype=float)
+        self.K2 = np.zeros((1,3),dtype=float)
+        self.K3 = np.zeros((1,3),dtype=float)
+        self.K4 = np.zeros((1,3),dtype=float)
+
+
+        self.X[0, 0] = -15.8;
+        self.X[0, 1] = -17.48;
+        self.X[0, 2] = 35.64;
+
+        self.sigma = 10
+        self.R = 24
+        self.b = -8/3
+
+
+    def step_update(self,step):
+        self.K1[0,0] = self.dx(self.X,step)
+        self.K1[0,1] = self.dy(self.X,step)
+        self.K1[0,2] = self.dz(self.X,step)
+
+        self.K2[0,0] = self.dx(self.X+self.K1/2,step)
+        self.K2[0,1] = self.dy(self.X+self.K1/2,step)
+        self.K2[0,2] = self.dz(self.X+self.K1/2,step)
+
+        self.K3[0,0] = self.dx(self.X+self.K2/2,step)
+        self.K3[0,1] = self.dy(self.X+self.K2/2,step)
+        self.K3[0,2] = self.dz(self.X+self.K2/2,step)
+
+        self.K4[0,0] = self.dx(self.X+self.K3,step)
+        self.K4[0,1] = self.dy(self.X+self.K3,step)
+        self.K4[0,2] = self.dz(self.X+self.K3,step)
+
+        self.X[step+1,:] = self.X[step,:] + (1/6)*(self.K1+2*self.K2+2*self.K3+self.K4)
+
+    def dx(self,x,step):
+        return self.dt * self.sigma * (x[step,1] - x[step,0])
+
+    def dy(self,x,step):
+        return self.dt * (self.R*x[step,0] - x[step,0]*x[step,2] - x[step,1])
+
+    def dz(self,x,step):
+        return self.dt * (x[step,0]*x[step,1] + self.b*x[step,2])
+
 
 # main
-dLz = lorenz()
+
+Lz = lorenz()
+
+for step in range(Lz.N-1):
+    Lz.step_update(step)
+
+fig = plt.figure()
+ax = fig.add_subplot(111,projection='3d')
+ax.plot(Lz.X[:,0],Lz.X[:,1],Lz.X[:,2])
+plt.show()
+# ax.plot(1,2,3)
