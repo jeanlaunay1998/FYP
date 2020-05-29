Introduce in system dynamics a_res. Fix all covariance matrices of MHEs (note that MHE with pre estimation is still not working. Convergence analysis is ready)

MHE_ballisic_reg.py
@@ -8,7 +8,7 @@ import sys
 
 
 class MHE_regularisation:
-    def __init__(self, model, observer, horizon_length, measurement_lapse, model_pen, opt_method = 'Gradient'):
+    def __init__(self, model, observer, horizon_length, measurement_lapse, model_pen, opt_method = 'Gradient', Q=[]):
         self.m = model  # copy direction of model to access all function of the class
         self.o = observer  # copy direction of observer to access all function of the class
 
@@ -26,12 +26,15 @@ class MHE_regularisation:
 
         self.x_solution = [0, 0, 0]
         self.mu1 = 1
-        self.mu2 = model_pen[6]
         self.matrixR = []
 
-        self.reg1 = LA.inv(np.array([[50, 0, 0], [0, (1e-2), 0], [0, 0, (1e-2)]]))
-        self.R_mu = np.identity(7)
-        for i in range(7): self.R_mu[i, i] = model_pen[i]
+        self.reg1 = LA.inv(np.array([[50, 0, 0], [0, (1e-3), 0], [0, 0, (1e-3)]]))
+        if Q == [] :
+            self.R_mu = np.identity(7)
+            for i in range(7): self.R_mu[i, i] = model_pen[i]
+        else:
+            self.R_mu = LA.inv(np.power(Q, 0.5))
+
         # -------------------------------------------------- #
         self.initial_coefs =[1,1,1,1,1]
         self.real_x = []
@@ -381,6 +384,5 @@ class MHE_regularisation:
         self.R[0] = res.x[1]
         self.R[1] = res.x[2]
         self.R[2] = res.x[3]
-        self.mu2 = res.x[4]
 
         print(res.x)

MHE_ballistic_MS.py
@@ -8,7 +8,7 @@ import sys
 
 
 class total_ballistic:
-    def __init__(self, model, observer, horizon_length, measurement_lapse, model_pen, opt_method = 'Newton LS'):
+    def __init__(self, model, observer, horizon_length, measurement_lapse, model_pen, opt_method = 'Newton LS', Q = []):
         self.m = model  # copy direction of model to access all function of the class
         self.o = observer  # copy direction of observer to access all function of the class
 
@@ -29,8 +29,12 @@ class total_ballistic:
         self.matrixR = []
 
         self.reg1 = LA.inv(np.array([[50, 0, 0], [0, (1e-3), 0], [0, 0, (1e-3)]]))
-        self.R_mu = np.identity(7)
-        for i in range(7): self.R_mu[i, i] = model_pen[i]
+        if Q == [] :
+            self.R_mu = np.identity(7)
+            for i in range(7): self.R_mu[i, i] = model_pen[i]
+        else:
+            self.R_mu = LA.inv(np.power(Q, 0.5))
+            self.mu2 = self.R_mu[6,6]
         # -------------------------------------------------- #
         self.initial_coefs =[1,1,1,1,1]
         self.real_x = []

integration.py
@@ -72,9 +72,9 @@ arrival = [1, 1]
 
 for i in range(len(N)):
     if MHE_type[i] == 'Total ballistic':
-        opt.append(total_ballistic(m, o, N[i], measurement_lapse, model_pen, method[i]))
+        opt.append(total_ballistic(m, o, N[i], measurement_lapse, model_pen, method[i], Q))
     elif MHE_type[i] == 'Ballistic reg':
-        opt.append(MHE_regularisation(m, o, N[i], measurement_lapse, model_pen, method[i]))
+        opt.append(MHE_regularisation(m, o, N[i], measurement_lapse, model_pen, method[i], Q))
     elif MHE_type[i] == 'Multi-shooting':
         opt.append(multishooting(m, d, o, N[i], measurement_lapse, measurement_pen, model_pen, Q, R, arrival[i], method[i]))
     elif MHE_type[i] == 'MS with PE':
@@ -164,7 +164,7 @@ while height > 5000 and t < t_lim:
                 # for j in range(len(opt[i].vars)):
                 #     opt[i].vars[j] = opt[i].vars[j]*(1+ np.random.normal(0, 0.1, 1)[0])
                 opt[i].estimation()
-                if MHE_type[i] == 'Total ballistic' or MHE_type[i] == 'Single shooting':
+                if MHE_type[i] == 'Total ballistic' or MHE_type[i] == 'Ballistic reg':
                     x = opt[i].last_state()
                     memory.save_data(t, x, o.h(m.r, 'off'), opt[i].cost(opt[i].vars), i)
                 else:

system_dynamics.py
@@ -46,7 +46,7 @@ class dynamics:
 
         # white noise accounting for not modeled physics
         self.delta_o = np.random.normal(0, pow(0.01*self.beta_o, 2), size=1)
-        self.a_res = 0 # np.random.normal(0, pow(0.01*LA.norm(self.a), 2), size=1) TO BE CHANGED
+        self.a_res = 0
         self.beta = self.beta_o + self.delta_o
         self.beta = self.beta.tolist()
 
@@ -134,6 +134,7 @@ class dynamics:
 
     def acceleration(self, v, r):
         acc = -np.multiply((self.G*self.M)/pow(LA.norm(r), 3), r) - np.multiply(self.density_h(r)*LA.norm(v)/(2*self.ballistic_coef(v, r)), v)
+        self.a_res = np.random.normal(0, pow(0.01 * LA.norm(acc), 2), size=1)
         return list(acc + self.a_res)
 
     def dx(self, v, r):